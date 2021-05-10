677d4f2d61179b33be811ea71046744dec155eef
Morten Linderud <morten@linderud.pw>
2021-05-10 18:24:02 +0200

    Move bin folder
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ .local/bin/start_jack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+jack_control start
+jack_control ds alsa
+jack_control dps device hw:HD2
+jack_control dps rate 48000
+jack_control dps nperiods 2
+jack_control dps period 64
+sleep 10
+qjackctl &
